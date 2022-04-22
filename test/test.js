741a0081fc04b6682782b3dcd8e1f90e1d294faf
AngelaCorr <acorriero@deakin.edu.au>
2022-04-22 19:00:52 +1000

    test case fix

--- test/test.js
+++ test/test.js
@@ -6,6 +6,9 @@ describe("Add Two Numbers", function() {
     it("returns status 200 to check if api works", function(done) {
         request(url, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
+            //console.log(response);
+            let bodyJson = JSON.parse(body);
+            expect(bodyJson.result).to.equal(8);
             done()
           });
     });
