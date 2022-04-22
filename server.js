3342bad96a00057cb6c4a0d2583c58af91276ba2
AngelaCorr <acorriero@deakin.edu.au>
2022-04-22 18:50:56 +1000

    adding test cases

--- server.js
+++ server.js
@@ -67,6 +67,15 @@ const cardList = [
 
     })
 
+    app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+        var firstNumber = parseInt(req.params.firstNumber) 
+        var secondNumber = parseInt(req.params.secondNumber)
+        var result = firstNumber + secondNumber || null
+        if(result == null) {
+          res.json({result: result, statusCode: 400}).status(400)
+        }
+        else { res.json({result: result, statusCode: 200}).status(200) } 
+      })
     
 app.use(express.static(__dirname+'/public'));
 app.use(cors)
